dca7403eeea2007d6fe618070de15bfef2654345
usmankhanX07 <us.afridi06@outlook.com>
2025-05-01 18:38:50 +0500

    Update Vehicle.java
    added more functions and took em out from the derived classes

--- Vehicle.java
+++ Vehicle.java
@@ -1,28 +1,45 @@
 package OOPProject;
 
+import java.awt.*;
+
 public abstract class Vehicle {
     protected int x, y;     //coordinates
     protected final int WIDTH;
     protected final int HEIGHT;
+    protected Image vehicleImage;
+    protected boolean isVehicleImageLoaded;
 
-    Vehicle(int width, int height, int x, int y){
+    Vehicle(int width, int height, int x, int y) {
         this.WIDTH = width;
         this.HEIGHT = height;
         this.x = x;
         this.y = y;
+        isVehicleImageLoaded = false;
     }
 
     public abstract void moveLeft();
+
     public abstract void moveRight();
+
     public abstract void moveUp();
+
     public abstract void moveDown();
 
-    public int getX(){
+    public int getX() {
         return x;
     }
 
-    public int getY(){
+    public int getY() {
         return y;
+    }
+
+    public void draw(Graphics g) {
+        if (vehicleImage != null && isVehicleImageLoaded) {
+            g.drawImage(vehicleImage, x, y, WIDTH, HEIGHT, null);
+        }
+    }
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, WIDTH, HEIGHT);
     }
 }
 
