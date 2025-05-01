54318ccf933e98e2979d26deda8ee55db65d5428
usmankhanX07 <us.afridi06@outlook.com>
2025-05-01 18:38:18 +0500

    Update Car.java
    added polymorphism and more functionality to the vehicle class

--- Car.java
+++ Car.java
@@ -1,55 +1,43 @@
-import java.awt.*;
+package OOPProject;
 import javax.swing.*;
 
 public class Car extends Vehicle{
 
-    private Image carImage;
-    private boolean useImage = false;  // Toggle this to true to use image instead of shapes
-
      Car(int startX, int startY) {
-         super(60,100, startX, startY);
-
-        // Load image (make sure car.png is in your project directory)
-        try {
-            carImage = new ImageIcon("C:\\Users\\k243032\\Desktop\\OOPProject\\car.jpg").getImage();
-            useImage = true;  // Set to true if image loads successfully
-        } catch (Exception e) {
-            System.out.println("Could not load car image. Using shape instead.");
-        }
+         super(50,100, startX, startY);
+         vehicleImage = new ImageIcon("C:\\Users\\irfan\\Desktop\\FAST\\OOPLab\\src\\OOPProject\\Pictures\\sportscar.png").getImage();
+         if (vehicleImage == null || vehicleImage.getWidth(null) == -1) {
+             isVehicleImageLoaded = false;
+         } else {
+             isVehicleImageLoaded = true;
+         }
     }
 
     public void moveLeft() {
         if (x > 200) {
-            x -= 20;
+            x -= 30;
         }
     }
 
     public void moveRight() {
-        if (x < 750 - WIDTH-200) {
-            x += 20;
+        if (x < 493) {
+            x += 30;
         }
     }
 
     public void moveUp() {
-        if (y > 0) {
-            y-=40;
+        if (y > 30) {
+            y-=30;
         }
     }
 
     public void moveDown() {
         if (y < 450) {
-            y+=40;
-        }
-    }
-    public void draw(Graphics g) {
-        if (useImage && carImage != null) {
-            g.drawImage(carImage, x, y, WIDTH, HEIGHT, null);
-        } else {
-            Graphics2D g2 = (Graphics2D) g;
+            y+=30;
         }
     }
 
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, WIDTH, HEIGHT);
+    public boolean didCarImageFailToLoad() {
+        return !(isVehicleImageLoaded);
     }
 }
