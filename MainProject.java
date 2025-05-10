b1c2bee584a7d2a357bcff75433add05e60b9455
usmankhanX07 <us.afridi06@outlook.com>
2025-05-10 17:56:39 +0500

    Update and rename Main.java to MainProject.java

---
+++ MainProject.java
@@ -0,0 +1,21 @@
+package OOPProject;
+import javax.swing.*;
+
+public class MainProject {
+    public static void main(String[] args) {
+        String difficulty = DifficultySelector.processDifficulty();
+
+//        ArrayList<Vehicle> vehicles = new ArrayList<>(5);
+        JFrame frame = new JFrame("High Speed Chase 2");
+        GamePanel panel = new GamePanel(difficulty);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);  // Set window size 
+        frame.setResizable(true);  // Disable resizing
+        frame.add(panel);
+        frame.setVisible(true);
+        panel.startGame();  // Start the game
+    }
+
+}
+
