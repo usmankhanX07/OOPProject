5e4c404c1bc19bd42ee255575c2923754551d932
usmankhanX07 <us.afridi06@outlook.com>
2025-05-13 21:58:17 +0500

    Update MainProject.java
    added difficulty selector as a static method which is then given to game panel in its constructor

--- MainProject.java
+++ MainProject.java
@@ -5,17 +5,15 @@ public class MainProject {
     public static void main(String[] args) {
         String difficulty = DifficultySelector.processDifficulty();
 
-//        ArrayList<Vehicle> vehicles = new ArrayList<>(5);
         JFrame frame = new JFrame("High Speed Chase 2");
         GamePanel panel = new GamePanel(difficulty);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);  // Set window size 
-        frame.setResizable(true);  // Disable resizing
+        frame.setSize(800, 800);
+        frame.setResizable(false);  // Disable resizing
         frame.add(panel);
         frame.setVisible(true);
-        panel.startGame();  // Start the game
+        panel.startGame();
     }
-
 }
 
